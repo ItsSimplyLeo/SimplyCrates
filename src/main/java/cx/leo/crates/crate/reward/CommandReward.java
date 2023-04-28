899f9b3d3da4431e97100dd9b1813f4becb12c94
TotalledZebra <Holappa57@gmail.com>
2023-04-28 21:32:26 +0300

    Allow multiple commands in CommandReward

--- src/main/java/cx/leo/crates/crate/reward/CommandReward.java
+++ src/main/java/cx/leo/crates/crate/reward/CommandReward.java
@@ -3,17 +3,18 @@ package cx.leo.crates.crate.reward;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
+import java.util.List;
 import java.util.regex.Pattern;
 
 public class CommandReward implements Reward {
 
     private static final Pattern PLAYER_PATTERN = Pattern.compile("%player%");
 
-    private final String command;
+    private final List<String> commands;
     private final double chance;
 
-    public CommandReward(String command, double chance) {
-        this.command = command;
+    public CommandReward(List<String> commands, double chance) {
+        this.commands = commands;
         this.chance = chance;
     }
 
@@ -22,13 +23,17 @@ public class CommandReward implements Reward {
         return chance;
     }
 
-    public String getCommand() {
-        return command;
+    public List<String> getCommands() {
+        return commands;
     }
 
     @Override
     public void give(Player player) {
-        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), PLAYER_PATTERN.matcher(getCommand()).replaceAll(player.getName()));
+
+        for (String command : getCommands()) {
+            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), PLAYER_PATTERN.matcher(command).replaceAll(player.getName()));
+        }
+
     }
 
 }
