e06919f2fe36da17eb85ef32923851f21f5e28ea
TotalledZebra <Holappa57@gmail.com>
2023-04-28 22:06:26 +0300

    Add support for multiple items in ItemReward

--- src/main/java/cx/leo/crates/crate/reward/ItemReward.java
+++ src/main/java/cx/leo/crates/crate/reward/ItemReward.java
@@ -3,13 +3,16 @@ package cx.leo.crates.crate.reward;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
+import java.util.List;
+import java.util.Map;
+
 public class ItemReward implements Reward {
 
-    private final ItemStack rewardItem;
+    private final List<ItemStack> rewardItems;
     private final double chance;
 
-    public ItemReward(ItemStack rewardItem, double chance) {
-        this.rewardItem = rewardItem;
+    public ItemReward(List<ItemStack> rewardItems, double chance) {
+        this.rewardItems = rewardItems;
         this.chance = chance;
     }
 
@@ -18,9 +21,21 @@ public class ItemReward implements Reward {
         return chance;
     }
 
+    public List<ItemStack> getItems() {
+        return rewardItems;
+    }
+
     @Override
     public void give(Player player) {
-        player.getInventory().addItem(rewardItem);
+
+        Map<Integer, ItemStack> leftOvers = player.getInventory().addItem(getItems().toArray(new ItemStack[0]));
+
+        if (!leftOvers.isEmpty()) {
+            for (ItemStack item : leftOvers.values()) {
+                player.getWorld().dropItemNaturally(player.getLocation(), item);
+            }
+        }
+
     }
 
 }
